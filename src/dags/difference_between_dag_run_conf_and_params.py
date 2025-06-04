a3e43f04b810a2921b1cd49b170eed7bfdcf58df
hiroki <hiroki_ado@icloud.com>
2025-06-05 00:23:52 +0900

    feat: difference between dag_run.conf and params

---
+++ src/dags/difference_between_dag_run_conf_and_params.py
@@ -0,0 +1,38 @@
+from datetime import UTC, datetime, timedelta
+
+from airflow.sdk import Param, dag, task
+
+"""
+scheduled
+params: 5
+conf: None
+
+manual without input
+params: 5
+conf: 5
+
+manual with 4
+params: 4
+conf: 4
+"""
+
+
+@dag(
+    start_date=datetime.now(UTC) + timedelta(days=-1),  # yesterday
+    schedule="* * * * *",
+    params={"hoge": Param(5, type="integer")},
+)
+def difference_between_dag_run_conf_and_params() -> None:
+    task1()
+
+
+@task
+def task1(**context) -> None:
+    import logging
+
+    logger = logging.getLogger(__name__)
+    logger.info(f"params: {context['params']['hoge']}")
+    logger.info(f"conf: {context['dag_run'].conf.get('hoge', None)}")
+
+
+difference_between_dag_run_conf_and_params()
